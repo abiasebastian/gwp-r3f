7503ceea70babe09da5c8e905e051905fbb09bbe
nandana56 <nandana.s56@gmail.com>
2026-02-15 17:36:32 +0530

    Add Login page component

---
+++ src/components/Login.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h2>Login Page</h2>
+      <p>This is a test login component.</p>
+    </div>
+  );
+};
+
+export default Login;
